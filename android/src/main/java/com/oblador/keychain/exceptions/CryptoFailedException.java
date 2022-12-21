14a27ca1d9e2300272b216787b0fef037bf27d82
Brian Yeh <brian.yeh@coolbitx.com>
2022-12-21 19:49:08 +0800

    fix: do not show device credential if user click cancel

--- android/src/main/java/com/oblador/keychain/exceptions/CryptoFailedException.java
+++ android/src/main/java/com/oblador/keychain/exceptions/CryptoFailedException.java
@@ -5,12 +5,24 @@ import androidx.annotation.Nullable;
 import java.security.GeneralSecurityException;
 
 public class CryptoFailedException extends GeneralSecurityException {
+  public @interface ErrorCode {
+    int UNKNOWN = -1;
+    int CANCELLED = 13;
+    int FAILED_MULTIPLE_TIMES = 7;
+  }
+  private int errorCode = ErrorCode.UNKNOWN;
+
   public CryptoFailedException(String message) {
     super(message);
   }
 
   public CryptoFailedException(String message, Throwable t) {
     super(message, t);
+  }
+
+  public CryptoFailedException(int errorCode, CharSequence errString) {
+    super("code: " + errorCode + ", msg: " + errString);
+    this.errorCode = errorCode;
   }
 
   public static void reThrowOnError(@Nullable final Throwable error) throws CryptoFailedException {
@@ -21,5 +33,9 @@ public class CryptoFailedException extends GeneralSecurityException {
 
     throw new CryptoFailedException("Wrapped error: " + error.getMessage(), error);
 
+  }
+
+  public int getErrorCode() {
+    return errorCode;
   }
 }
